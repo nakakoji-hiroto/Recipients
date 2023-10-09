2d720e8594ca54cb004ed90f5baa21f9413ba8e1
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-10-09 05:11:16 +0000

    feature login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)(?=.*?[\W_])[!-~]{8,}+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'パスワードは半角英数記号を各1文字以上含める必要があります' }
+         
+  GUEST_USER_EMAIL = "guest@example.com"
+  
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
+  
+  scope module: :admin do
+    devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
   }
+  end
 
-  devise_for :users, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
+  scope module: :public do
+    root to: 'homes#top'
+    devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+    }
+    devise_scope :user do
+    post "users/guest_sign_in", to: "sessions#guest_sign_in"
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
