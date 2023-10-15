35d787e9d1c47bd2a54a5a6d7b1d50bdb8a6f685
nakakoji-hiroto <140766730+nakakoji-hiroto@users.noreply.github.com>
2023-10-15 13:46:31 +0000

    Add review_sort

--- app/controllers/public/recipe_comments_controller.rb
+++ app/controllers/public/recipe_comments_controller.rb
@@ -1,13 +1,13 @@
 class Public::RecipeCommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_guest_user,except: [:index, :show]
-  
+
   def new
     @recipe_comment = RecipeComment.new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_tags = @recipe.tags
   end
-  
+
   def create
     @recipe_comment = current_user.recipe_comments.new(recipe_comment_params)
     @recipe_comment.recipe_id = params[:recipe_id]
@@ -20,27 +20,41 @@ class Public::RecipeCommentsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def index
     @recipe = Recipe.find(params[:recipe_id])
-    @recipe_comments = RecipeComment.all
+    @recipe_comment = RecipeComment.new
+    @choices = {評価の高い順に: 'high_rating', 日付の新しい順に: 'new', 日付の古い順に: 'old'}
+    sort = params[:sort]
+    case sort
+      when 'high_rating'
+        @recipe_comments = @recipe.recipe_comments.order('score DESC')
+      when 'new'
+        @recipe_comments = @recipe.recipe_comments.order('id DESC')
+        @choices = {日付の新しい順に: 'new', 日付の古い順に: 'old', 評価の高い順に: 'high_rating'}
+      when 'old'
+        @recipe_comments = @recipe.recipe_comments.order('id ASC')
+        @choices = {日付の古い順に: 'old',  評価の高い順に: 'high_rating', 日付の新しい順に: 'new'} 
+      else
+        @recipe_comments = @recipe.recipe_comments
+    end
   end
-  
+
   def show
     @recipe_comment = RecipeComment.find(params[:id])
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_tags = @recipe.tags
   end
-  
+
   def destroy
     recipe_comment = RecipeComment.find(params[:id])
     recipe_comment.delete
     flash[:notice] = "レビューを削除しました"
     redirect_to recipe_path(params[:recipe_id])
   end
-  
+
   private
-  
+
   def recipe_comment_params
     params.require(:recipe_comment).permit(:comment, :score, :is_release)
   end
